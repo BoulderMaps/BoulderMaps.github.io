c53edd56aee4ba31adca3520b154d2b5d0339f26
BoulderMaps <99031780+BoulderMaps@users.noreply.github.com>
2022-05-02 19:57:11 +0100

    Update Map.py

--- Map.py
+++ Map.py
@@ -2,7 +2,7 @@ import pandas as pd
 import folium
 from folium.plugins import LocateControl
 
-m = folium.Map(location=[45.5236, -1.6750])
+m = folium.Map(location=[55.192192, -1.941678])
 LocateControl().add_to(m)
 
 m.save('m.html')
